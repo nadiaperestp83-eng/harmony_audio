24a6a9a9c68d35ce5b53692c416918a3cd5189fa
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-07-22 13:32:22 -0300

    Create harmony_audio.dart

---
+++ lib/harmony_audio.dart
@@ -0,0 +1,12 @@
+/// harmony_audio
+///
+/// Pacote de dados e assets para o app de música: centraliza músicas
+/// espirituais (.mp3) e poesias narradas (Internet Archive / LibriVox),
+/// com models tipados e um repositório único de acesso.
+library harmony_audio;
+
+export 'src/models/content_category.dart';
+export 'src/models/content_item.dart';
+export 'src/models/song_item.dart';
+export 'src/models/poem_item.dart';
+export 'src/repository/content_repository.dart';
